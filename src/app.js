6b81d21b7a966fbcce39200b8c0550a76bda996d
Sarunk21 <kautsarmkp@gmail.com>
2022-06-13 23:25:33 +0700

    Linked list dan sorting

--- src/app.js
+++ src/app.js
@@ -3,42 +3,43 @@ const products = JSON.parse(localStorage.getItem('products')) || [];
 const couriers = JSON.parse(localStorage.getItem('couriers')) || [];
 const customers = JSON.parse(localStorage.getItem('customers')) || [];
 const transactions = JSON.parse(localStorage.getItem('transactions')) || [];
+const sortJarak = JSON.parse(localStorage.getItem('customers')) || [];
 
 // Produk
 
-    // Add Produk
-    function addProduk(nama, harga, stok) {
-        // Get last id
-        const data = JSON.parse(localStorage.getItem('products'));
-        let id = 1;
-        if (data && data.length > 0) {
-            id = data[data.length - 1][0] + 1;
-        }
-
-        const newProduk = [id, nama, parseInt(harga), parseInt(stok)];
-        products.push(newProduk);
-
-        // Save to localstorage
-        localStorage.setItem('products', JSON.stringify(products));
-
-        // Refresh table
-        getProduk();
+// Add Produk
+function addProduk(nama, harga, stok) {
+    // Get last id
+    const data = JSON.parse(localStorage.getItem('products'));
+    let id = 1;
+    if (data && data.length > 0) {
+        id = data[data.length - 1][0] + 1;
     }
 
-    // Get Produk
-    function getProduk() {
-        const tableProduk = $('#dataProduk')
+    const newProduk = [id, nama, parseInt(harga), parseInt(stok)];
+    products.push(newProduk);
 
-        // Clear table
-        tableProduk.html('');
+    // Save to localstorage
+    localStorage.setItem('products', JSON.stringify(products));
 
-        // Get data from localstorage
-        const data = JSON.parse(localStorage.getItem('products'));
+    // Refresh table
+    getProduk();
+}
 
-        if (data) {
-            // Loop data
-            data.forEach(item => {
-                const row = `
+// Get Produk
+function getProduk() {
+    const tableProduk = $('#dataProduk')
+
+    // Clear table
+    tableProduk.html('');
+
+    // Get data from localstorage
+    const data = JSON.parse(localStorage.getItem('products'));
+
+    if (data) {
+        // Loop data
+        data.forEach(item => {
+            const row = `
                     <tr>
                         <td>${item[0]}</td>
                         <td>${item[1]}</td>
@@ -49,66 +50,66 @@ const transactions = JSON.parse(localStorage.getItem('transactions')) || [];
                         </td>
                     </tr>
                 `;
-                tableProduk.append(row);
-            });
-        }
+            tableProduk.append(row);
+        });
+    }
+}
+
+// Delete Produk
+function deleteProduk(id) {
+    if (confirm('Apakaah anda yakin?')) {
+        // Get data from localstorage
+        const data = JSON.parse(localStorage.getItem('products'));
+
+        // Find index
+        const index = data.findIndex(item => item[0] == id);
+
+        // Delete data
+        data.splice(index, 1);
+
+        // Save to localstorage
+        localStorage.setItem('products', JSON.stringify(data));
     }
 
-    // Delete Produk
-    function deleteProduk(id) {
-        if (confirm('Apakaah anda yakin?')) {
-            // Get data from localstorage
-            const data = JSON.parse(localStorage.getItem('products'));
-    
-            // Find index
-            const index = data.findIndex(item => item[0] == id);
-    
-            // Delete data
-            data.splice(index, 1);
-    
-            // Save to localstorage
-            localStorage.setItem('products', JSON.stringify(data));
-        }
-
-        // Refresh table
-        getProduk();
-    }
+    // Refresh table
+    getProduk();
+}
 
 // Kurir
 
-    // Add Kurir
-    function addKurir(nama) {
-        // Get last id
-        const data = JSON.parse(localStorage.getItem('couriers'));
-        let id = 1;
-        if (data && data.length > 0) {
-            id = data[data.length - 1][0] + 1;
-        }
-
-        const newKurir = [id, nama];
-        couriers.push(newKurir);
-
-        // Save to localstorage
-        localStorage.setItem('couriers', JSON.stringify(couriers));
-
-        // Refresh table
-        getKurir();
+// Add Kurir
+function addKurir(nama) {
+    // Get last id
+    const data = JSON.parse(localStorage.getItem('couriers'));
+    let id = 1;
+    if (data && data.length > 0) {
+        id = data[data.length - 1][0] + 1;
     }
 
-    // Get Kurir
-    function getKurir() {
-        const tableKurir = $('#dataKurir')
+    const newKurir = [id, nama];
+    couriers.push(newKurir);
 
-        // Clear table
-        tableKurir.html('');
+    // Save to localstorage
+    localStorage.setItem('couriers', JSON.stringify(couriers));
 
-        // Get data from localstorage
-        const data = JSON.parse(localStorage.getItem('couriers'));
+    // Refresh table
+    getKurir();
+}
 
-        if (data) {
-            // Loop data
-            data.forEach(item => {
-                const row = `
+// Get Kurir
+function getKurir() {
+    const tableKurir = $('#dataKurir')
+
+    // Clear table
+    tableKurir.html('');
+
+    // Get data from localstorage
+    const data = JSON.parse(localStorage.getItem('couriers'));
+
+    if (data) {
+        // Loop data
+        data.forEach(item => {
+            const row = `
                     <tr>
                         <td>${item[0]}</td>
                         <td>${item[1]}</td>
@@ -117,66 +118,66 @@ const transactions = JSON.parse(localStorage.getItem('transactions')) || [];
                         </td>
                     </tr>
                 `;
-                tableKurir.append(row);
-            });
-        }
+            tableKurir.append(row);
+        });
+    }
+}
+
+// Delete Kurir
+function deleteKurir(id) {
+    if (confirm('Apakaah anda yakin?')) {
+        // Get data from localstorage
+        const data = JSON.parse(localStorage.getItem('couriers'));
+
+        // Find index
+        const index = data.findIndex(item => item[0] == id);
+
+        // Delete data
+        data.splice(index, 1);
+
+        // Save to localstorage
+        localStorage.setItem('couriers', JSON.stringify(data));
     }
 
-    // Delete Kurir
-    function deleteKurir(id) {
-        if (confirm('Apakaah anda yakin?')) {
-            // Get data from localstorage
-            const data = JSON.parse(localStorage.getItem('couriers'));
-    
-            // Find index
-            const index = data.findIndex(item => item[0] == id);
-    
-            // Delete data
-            data.splice(index, 1);
-    
-            // Save to localstorage
-            localStorage.setItem('couriers', JSON.stringify(data));
-        }
-
-        // Refresh table
-        getKurir();
-    }
+    // Refresh table
+    getKurir();
+}
 
 // Customer
 
-    // Add Customer
-    function addCustomer(nama, notelp, alamat, jarak) {
-        // Get last id
-        const data = JSON.parse(localStorage.getItem('customers'));
-        let id = 1;
-        if (data && data.length > 0) {
-            id = data[data.length - 1][0] + 1;
-        }
-
-        const newCustomer = [id, nama, notelp, alamat, parseInt(jarak)];
-        customers.push(newCustomer);
-
-        // Save to localstorage
-        localStorage.setItem('customers', JSON.stringify(customers));
-
-        // Refresh table
-        getCustomer();
+// Add Customer
+function addCustomer(nama, notelp, alamat, jarak) {
+    // Get last id
+    const data = JSON.parse(localStorage.getItem('customers'));
+    let id = 1;
+    if (data && data.length > 0) {
+        id = data[data.length - 1][0] + 1;
     }
 
-    // Get Customer
-    function getCustomer() {
-        const tableCustomer = $('#dataCustomer')
+    const newCustomer = [id, nama, notelp, alamat, parseInt(jarak)];
+    customers.push(newCustomer);
 
-        // Clear table
-        tableCustomer.html('');
+    // Save to localstorage
+    localStorage.setItem('customers', JSON.stringify(customers));
 
-        // Get data from localstorage
-        const data = JSON.parse(localStorage.getItem('customers'));
+    // Refresh table
+    getCustomer();
+}
 
-        if (data) {
-            // Loop data
-            data.forEach(item => {
-                const row = `
+// Get Customer
+function getCustomer() {
+    const tableCustomer = $('#dataCustomer')
+
+    // Clear table
+    tableCustomer.html('');
+
+    // Get data from localstorage
+    const data = JSON.parse(localStorage.getItem('customers'));
+
+    if (data) {
+        // Loop data
+        data.forEach(item => {
+            const row = `
                     <tr>
                         <td>${item[0]}</td>
                         <td>${item[1]}</td>
@@ -188,79 +189,83 @@ const transactions = JSON.parse(localStorage.getItem('transactions')) || [];
                         </td>
                     </tr>
                 `;
-                tableCustomer.append(row);
-            });
-        }
+            tableCustomer.append(row);
+        });
+    }
+}
+
+// Delete Customer
+function deleteCustomer(id) {
+    if (confirm('Apakaah anda yakin?')) {
+        // Get data from localstorage
+        const data = JSON.parse(localStorage.getItem('customers'));
+
+        // Find index
+        const index = data.findIndex(item => item[0] == id);
+
+        // Delete data
+        data.splice(index, 1);
+
+        // Save to localstorage
+        localStorage.setItem('customers', JSON.stringify(data));
     }
 
-    // Delete Customer
-    function deleteCustomer(id) {
-        if (confirm('Apakaah anda yakin?')) {
-            // Get data from localstorage
-            const data = JSON.parse(localStorage.getItem('customers'));
-    
-            // Find index
-            const index = data.findIndex(item => item[0] == id);
-    
-            // Delete data
-            data.splice(index, 1);
-    
-            // Save to localstorage
-            localStorage.setItem('customers', JSON.stringify(data));
-        }
-
-        // Refresh table
-        getCustomer();
-    }
+    // Refresh table
+    getCustomer();
+}
 
 
 // Transaksi
 
-    // Add Transaksi
-    function addTransaksi(id_customer, id_kurir, id_produk, jumlah) {
-        // Get last id
-        const data = JSON.parse(localStorage.getItem('transactions'));
-        let id = 1;
-        if (data && data.length > 0) {
-            id = data[data.length - 1][0] + 1;
-        }
+// Add Transaksi
+function addTransaksi(id_customer, id_kurir, id_produk, jumlah) {
+    // Data
+    const dataProduk = products.filter(item => item[0] == id_produk);
+    const dataCustomer = customers.filter(item => item[0] == id_customer);
 
-        // Total harga
-        const totalHarga = products.filter(item => item[0] == id_produk)[0][2] * jumlah;
-
-        const newTransaksi = [id, id_customer, id_kurir, id_produk, jumlah, totalHarga];
-        transactions.push(newTransaksi);
-
-        // Kurangin stok produk
-        const index = products.filter(item => item[0] == id_produk)[0][0] - 1;
-        products[index][3] -= jumlah;
-
-        // Save to localstorage
-        localStorage.setItem('transactions', JSON.stringify(transactions));
-        localStorage.setItem('products', JSON.stringify(products));
-
-        // Refresh table
-        getTransaksi();
+    // Get last id
+    const data = JSON.parse(localStorage.getItem('transactions'));
+    let id = 1;
+    if (data && data.length > 0) {
+        id = data[data.length - 1][0] + 1;
     }
 
-    // Get Transaksi
-    function getTransaksi() {
-        const tableTransaksi = $('#dataTransaksi')
+    // Total harga
+    const totalHarga = (dataProduk[0][2] * jumlah) + (dataCustomer[0][4] > 8 ? dataCustomer[0][4] * 15000 : dataCustomer[0][4] * 7000);
 
-        // Clear table
-        tableTransaksi.html('');
+    const newTransaksi = [id, id_customer, id_kurir, id_produk, jumlah, totalHarga];
+    transactions.push(newTransaksi);
 
-        // Get data from localstorage
-        const data = JSON.parse(localStorage.getItem('transactions'));
+    // Kurangin stok produk
+    const index = dataProduk[0][0] - 1;
+    products[index][3] -= jumlah;
 
-        if (data) {
-            // Loop data
-            data.forEach(item => {
-                const namaCustomer = customers.filter(item2 => item2[0] == item[1])[0][1];
-                const namaKurir = couriers.filter(item2 => item2[0] == item[2])[0][1];
-                const namaProduk = products.filter(item2 => item2[0] == item[3])[0][1];
+    // Save to localstorage
+    localStorage.setItem('transactions', JSON.stringify(transactions));
+    localStorage.setItem('products', JSON.stringify(products));
 
-                const row = `
+    // Refresh table
+    getTransaksi();
+}
+
+// Get Transaksi
+function getTransaksi() {
+    const tableTransaksi = $('#dataTransaksi')
+
+    // Clear table
+    tableTransaksi.html('');
+
+    // Get data from localstorage
+    const data = JSON.parse(localStorage.getItem('transactions'));
+
+    if (data) {
+        // Loop data
+        data.forEach(item => {
+            const namaCustomer = customers.filter(item2 => item2[0] == item[1])[0][1];
+            const namaKurir = couriers.filter(item2 => item2[0] == item[2])[0][1];
+            const namaProduk = products.filter(item2 => item2[0] == item[3])[0][1];
+
+            const row = `
                     <tr>
                         <td>${item[0]}</td>
                         <td>${namaCustomer}</td>
@@ -273,28 +278,145 @@ const transactions = JSON.parse(localStorage.getItem('transactions')) || [];
                         </td>
                     </tr>
                 `;
-                tableTransaksi.append(row);
-            });
+            tableTransaksi.append(row);
+        });
+    }
+}
+
+// Delete Transaks
+function deleteTransaksi(id) {
+    if (confirm('Apakaah anda yakin?')) {
+        // Get data from localstorage
+        const data = JSON.parse(localStorage.getItem('transactions'));
+
+        // Find index
+        const index = data.findIndex(item => item[0] == id);
+
+        // Delete data
+        data.splice(index, 1);
+
+        // Save to localstorage
+        localStorage.setItem('transactions', JSON.stringify(data));
+    }
+
+    // Refresh table
+    getTransaksi();
+}
+
+// Sorting jarak toko ke customer
+function sortingJarak() {
+    // Sort data
+    // Bubble sort
+    for (let i = 0; i < sortJarak.length; i++) {
+
+        for (let j = 0; j < (sortJarak.length - i - 1); j++) {
+
+            if (sortJarak[j][4] > sortJarak[j + 1][4]) {
+
+                let temp = sortJarak[j]
+                sortJarak[j] = sortJarak[j + 1]
+                sortJarak[j + 1] = temp
+            }
         }
     }
 
-    // Delete Transaksi
-    function deleteTransaksi(id) {
-        if (confirm('Apakaah anda yakin?')) {
-            // Get data from localstorage
-            const data = JSON.parse(localStorage.getItem('transactions'));
+    const tableCustomer = $('#sortedCustomer')
 
-            // Find index
-            const index = data.findIndex(item => item[0] == id);
+    // Clear table
+    tableCustomer.html('');
 
-            // Delete data
-            data.splice(index, 1);
+    if (sortJarak && sortJarak.length > 0) {
+        // Loop data
+        sortJarak.forEach(item => {
+            const row = `
+                    <tr>
+                        <td>${item[0]}</td>
+                        <td>${item[1]}</td>
+                        <td>${item[2]}</td>
+                        <td>${item[3]}</td>
+                        <td>${item[4]}</td>
+                    </tr>
+                `;
+            tableCustomer.append(row);
+        });
+    }
+}
 
-            // Save to localstorage
-            localStorage.setItem('transactions', JSON.stringify(data));
-        }
+sortingJarak();
 
-        // Refresh table
-        getTransaksi();
+// linked list
+class Node {
+    constructor(element) {
+        this.element = element;
+        this.next = null
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
     }
 
+    add(element) {
+        let node = new Node(element);
+        let current;
+
+        if (this.head == null)
+            this.head = node;
+        else {
+            current = this.head;
+
+            while (current.next) {
+                current = current.next;
+            }
+
+            current.next = node;
+        }
+        this.size++;
+    }
+
+    isEmpty() {
+        return this.size == 0;
+    }
+
+    size() {
+        return this.size;
+    }
+
+    getFirst() {
+        return this.head;
+    }
+
+    getLast() {
+        if (this.head == null)
+            return null;
+
+        let current = this.head;
+
+        while (current.next) {
+            current = current.next;
+        }
+
+        return current;
+    }
+}
+
+// Create linked list
+let li = new LinkedList();
+
+for (let i = 0; i < sortJarak.length; i++) {
+    li.add(sortJarak[i]);
+}
+
+function getTotalCustomer() {
+    alert('Total customer: ' + li.size);
+}
+
+function getFirstCustomer() {
+    alert(`First customer: ${li.getFirst().element[1]}`);
+}
+
+function getLastCustomer() {
+    alert(`Last customer: ${li.getLast().element[1]}`);
+}
